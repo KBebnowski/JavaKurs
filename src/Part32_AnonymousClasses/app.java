68fc468881d41ed3eafa3487319248c0e646e5bb
Kbebnowski <Bebnowski.kamil@gmail.com>
2019-10-20 17:34:11 +0200

    commit

--- src/Part32_AnonymousClasses/app.java
+++ src/Part32_AnonymousClasses/app.java
@@ -4,6 +4,9 @@ class Machine{
     public void start() {
         System.out.println("Machine starting");
     }
+  public void stop(){
+      System.out.println("stop");
+  }
 }
 
 interface Plant{
@@ -22,7 +25,8 @@ public class app {
 
     public static void main(String[] args) {
 
-    //Anonimowa klasa to klasa ktora nie moze miec stworzonego obiektu, moze byc uzyta tak jak w tym przypadku do przeciazenia jednej metody
+    //Anonimowa klasa przeciaza jakas metode juz wczesniej stworzonej klasy. Nie jest ona obiektem tej klasy lecz jakims jej dzieckiem.
+        // W interfejsach nie mozna tworzyc obiektu, chyba ze tworzysz inna klase ktora extends, dziedziczy po klasie interfejs, jednak jesli nie chcesz tworzyc nowej klasy tylko po to to uzwasz klasy anonimowej
         Machine machine = new Machine(){ //to juz nie jest klasa Machine, to jakies dziecko klasy Machine, poniewaz metoda start jest inna.
             @Override                    //Klasa ta nie ma nazwy, dlatego nazywa sie anonimowa
             public void start() {
@@ -30,6 +34,7 @@ public class app {
             }
         };
         machine.start();//przeciazy metode tylko w obiekcie machine
+        machine.stop();
 
         Machine machine2 = new Machine();
         machine2.start();//zwykla metoda klasy Machine
@@ -50,7 +55,11 @@ public class app {
         };
         plant.grow();//wywolanie metody
 
-
+//wyrazenia lambda to skrocenie czytelnosci kodu klasy anonimowej
+Plant plant2 =() ->{
+        System.out.println("goerge");
+};
+plant2.grow();
 
     }
 
